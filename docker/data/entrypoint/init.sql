c6a9518b7525e1b02de7fce28c6d361cf342982c
Rheidi Waga <rheidiwaga@hotmail.com>
2025-01-23 16:30:48 -0300

    Criacao de tabela de ingredientes

--- docker/data/entrypoint/init.sql
+++ docker/data/entrypoint/init.sql
@@ -1,3 +1,12 @@
 CREATE SCHEMA restaurant;
 ALTER DATABASE restaurant SET search_path TO restaurant;
 DROP SCHEMA "public";
+
+CREATE TABLE ingredients(
+    id UUID PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    quantity INTEGER NOT NULL,
+    price NUMERIC (5,2) NOT NULL,
+    created_date TIMESTAMPTZ NOT NULL,
+    last_modified_date TIMESTAMPTZ NOT NULL
+);
