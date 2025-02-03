90715d3abf637d370544963ad463c6d3630b8235
Rheidi Waga <rheidiwaga@hotmail.com>
2025-02-03 09:21:12 -0300

    [Service] Implementação da lógica de listagem de ingredientes #2

--- src/main/java/com/backend/restaurant/controller/IngredientController.java
+++ src/main/java/com/backend/restaurant/controller/IngredientController.java
@@ -1,5 +1,6 @@
 package com.backend.restaurant.controller;
 import com.backend.restaurant.model.Ingredient;
+import com.backend.restaurant.service.IngredientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +11,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/ingredients")
-public class IngredientController {
+public class IngredientController{
+
+    private final IngredientService ingredientService;
+
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
 
     @GetMapping
     public ResponseEntity<List<Ingredient>> getAllIngredients() {
-        return new ResponseEntity<>(Collections.emptyList(), HttpStatus.OK);
+        List<Ingredient> ingredients = ingredientService.getAllIngredients();
+        return new ResponseEntity<>(ingredients, HttpStatus.OK);
     }
 }
