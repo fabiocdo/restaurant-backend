6f4fdfcd59aefb48aa6c28e4885ee514386d7856
Rheidi Waga <rheidiwaga@hotmail.com>
2025-08-26 11:35:34 -0300

    R-6 adicionado devolver lista de ingredientes (vazia) e devolver ingredientes por id

--- src/main/java/com/backend/restaurant/controller/IngredientController.java
+++ src/main/java/com/backend/restaurant/controller/IngredientController.java
@@ -7,8 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;//esse
 
 import java.util.List;
+import java.util.UUID;//esse
+import java.util.Collections;//esse
 
 @RestController
 @RequestMapping("/ingredients")
@@ -20,9 +23,20 @@ public class IngredientController {
         this.ingredientService = ingredientService;
     }
 
-    @GetMapping
+    @GetMapping("/getAllIngredients") //mudei pra chamar todos, sem esse caminho nao consegui fazer funcionar
     public ResponseEntity<List<Ingredient>> getAllIngredients() {
         List<Ingredient> ingredients = ingredientService.getAllIngredients();
         return new ResponseEntity<>(ingredients, HttpStatus.OK);
+    }
+    @GetMapping(params = "id")
+    public ResponseEntity<UUID> getIngredientById(@RequestParam UUID id) {
+        UUID randomId = UUID.randomUUID();// enquanto não temos service/repository, devolvemos UUID random
+        return new ResponseEntity<>(randomId, HttpStatus.OK);
+    }
+
+    @GetMapping(params = "name")
+    public ResponseEntity<List<Ingredient>> getIngredientByName(@RequestParam String name) {
+        return new ResponseEntity<>(Collections.emptyList(), HttpStatus.OK);
+        // enquanto não temos service/repository, devolvemos lista vazia emptyList()
     }
 }
