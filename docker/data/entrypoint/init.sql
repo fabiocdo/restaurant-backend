8b86b8889296e4cf12206cd19b8ae7088f71300d
Rheidi Waga <rheidiwaga@hotmail.com>
2025-12-09 17:10:28 -0300

    tables -> recipe and ingredient_recipe created

--- docker/data/entrypoint/init.sql
+++ docker/data/entrypoint/init.sql
@@ -11,3 +11,19 @@ CREATE TABLE ingredients
     created_date       TIMESTAMPTZ   NOT NULL,
     last_modified_date TIMESTAMPTZ   NOT NULL
 );
+
+CREATE TABLE recipe
+(
+    id                 UUID PRIMARY KEY,
+    name               VARCHAR       NOT NULL,
+    total_price        NUMERIC(5, 2) NOT NULL  DEFAULT 0.00,
+    created_date       TIMESTAMPTZ   NOT NULL,
+    last_modified_date TIMESTAMPTZ   NOT NULL
+);
+
+CREATE TABLE ingredient_recipe
+(
+    fk_ingredient_id UUID REFERENCES ingredients(id),
+    fk_recipe_id     UUID REFERENCES recipe(id),
+    ingredient_quantity INTEGER NOT NULL
+);
